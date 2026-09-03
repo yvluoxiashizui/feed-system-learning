83e981d897b5318a1bfc436ea4d2723d4774911d
yvluoxiashizui <dhyamniam946@gmail.com>
2026-09-03 16:24:36 +0800

    feat: 关注功能（防重复关注）

---
+++ handlers/follow.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"feed/models"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func FollowUser(c *gin.Context) {
+	userID := c.GetString("user_id")
+	uid, _ := strconv.ParseUint(userID, 10, 64)
+
+	var input struct {
+		VloggerID uint `json:"vlogger_id"`
+	}
+	c.ShouldBindJSON(&input)
+
+	follow := models.Follow{
+		FollowerID: uint(uid),
+		VloggerID:  input.VloggerID,
+	}
+
+	if err := db.Create(&follow).Error; err != nil {
+		c.JSON(400, gin.H{"error": "不能重复关注！"})
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "关注成功"})
+}
